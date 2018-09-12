bf34ad9cd443ac998daa5e9a05fa2cb263de35de
kiran5july <kiran_wp@yahoo.com>
2018-09-12 17:45:19 -0500

    Update delete_old_event_history_hdfs.sh

--- delete_old_event_history_hdfs.sh
+++ delete_old_event_history_hdfs.sh
@@ -4,12 +4,13 @@
 #1. VISION_HOME path (to get conf/vision.conf file)
 #2.
 
+VISION_HOME=$1
+
 processDate=`date '+%Y%m%d%H%M%S'`
 FROM_EMAIL="kmiry@ABC.com"
 ALERT_EMAIL="kmiry@ABC.com"
 ALERT_EMAIL="kmiry@ABC.com"
 LOG_DIR=/ABC/log
-VISION_HOME=$1
 ERROR_MSG=""
 
 # Define the functions
@@ -29,15 +30,13 @@ log() {
 #Check for the input
 if [ -z "$VISION_HOME" ]; then
 	ERROR_MSG="ERROR: Arg#1 for Vision Home path is a requred argument."
-	echo "$ERROR_MSG"
 	log_and_alert "$ERROR_MSG" "Missing Required argument"
 	exit 1
 else
 	if [ -d "$VISION_HOME" ]; then
 		if [ -f "$VISION_HOME/conf/vision.conf" ]; then
 			HDFSEventsBaseLoc=$(cat $VISION_HOME/conf/vision.conf | grep "vision.factSpan.rawDataLocation" | awk '{print $3}' | tr -d '\r')
-			#HDFSEventsBaseLoc=$(echo ${HDFSEventsBaseLoc} | tr -d '\r')
-			echo "Raw data location in HDFS: $HDFSEventsBaseLoc"
+			log "Raw data location in HDFS: $HDFSEventsBaseLoc"
 			if [ -z "$HDFSEventsBaseLoc" ]; then
 				ERROR_MSG="ERROR: Raw/Event Data Location property is not set in config file: $VISION_HOME/conf/vision.conf"
 				log_and_alert "$ERROR_MSG" "Missing property in config file"
@@ -58,30 +57,31 @@ fi
 export KEYTAB_FILE="/home/kmiry/kmiry.keytab"
 export HDFS_PRINCIPAL="kmiry@AD.ABC.COM"
 
-#kinit -k -t ${KEYTAB_FILE} ${HDFS_PRINCIPAL}
-#if [ $? != 0 ]
-#then
-#    # Failed to get a ticket. Log accordingly and exit!
-#    log "No valid Kerberos ticket available."
-#    exit 1
-#fi
-#log "Got the kerberos key."
+kinit -k -t ${KEYTAB_FILE} ${HDFS_PRINCIPAL}
+if [ $? != 0 ]
+then
+    # Failed to get a ticket. Log accordingly and exit!
+    log "No valid Kerberos ticket available."
+    exit 1
+fi
+log "Got the kerberos key."
 
 export FROM_EMAIL="kmiry@ABC.com"
 export ALERT_EMAIL="kmiry@ABC.com"
 export LOG_DIR=/ABC/log
 
-iYear=$(date -d '7 month ago' '+%Y')
-iMonth=$(date -d '7 month ago' '+%-m')
-#echo "Year=$iYear, Month=$iMonth"
+iYear=$(date -d '7 months ago' '+%Y')
+iMonth=$(date -d '7 months ago' '+%-m')
 
-HDFSMonthYearDir="/year=${iYear}/month=${iMonth}"
-#echo "Partition path= $HDFSMonthYearDir"
+log "Deleting partition path= ${HDFSEventsBaseLoc}/year=${iYear}/month=${iMonth}"
+hdfs dfs -rm -R -f "${HDFSEventsBaseLoc}/year=${iYear}/month=${iMonth}"
 
-#HDFSDirToDelete=$HDFSEventsBaseLoc$HDFSMonthYearDir
-#echo $HDFSEventsBaseLoc$HDFSMonthYearDir
+#Another month older as well
+iYear=$(date -d '8 months ago' '+%Y')
+iMonth=$(date -d '8 months ago' '+%-m')
 
-hdfs dfs -rm -R $HDFSEventsBaseLoc$HDFSMonthYearDir
+log "Deleting partition path= ${HDFSEventsBaseLoc}/year=${iYear}/month=${iMonth}"
+hdfs dfs -rm -R -f "${HDFSEventsBaseLoc}/year=${iYear}/month=${iMonth}"
 
 returnCode=$?
 if [ ${returnCode} != 0 ]
