62fe4d58edcbbb4c89945d97cbada88b78adf083
kmbeyond <kiran.learn7@gmail.com>
2020-01-28 16:53:02 -0500

    Update hive_query_loop_data.sh

--- hive_query_loop_data.sh
+++ hive_query_loop_data.sh
@@ -9,7 +9,7 @@ cg_req_data=$(beeline -u "${hive_url}" --silent=true --showHeader=false --output
  -e "set mapreduce.job.queuename=${pool_name}; SELECT req.id,req.tpenddate,req.merchants,ra.audience_id FROM db.cg_request req JOIN db.cg_request_audience ra ON ra.id=req.id AND ra.process_date=req.process_date WHERE req.process_date IN (SELECT MAX(b.process_date) FROM db.cg_request b)")
 cg_req_data=$(echo $cg_req_data | sed -r "s/'//g")
 
-for str in ${cg_req_data// / }; do
+for str in "${cg_req_data// / }"; do
    sId=$(cut -f1 -d',' <<< "$str");
    sTPEnddate=$(cut -f2 -d',' <<< "$str");
    sMerchants=$(cut -f3 -d',' <<< "$str");
