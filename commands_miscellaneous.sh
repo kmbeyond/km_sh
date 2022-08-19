9dcbfc91aa6b9cc6c4ea1a68797d76f35604f42f
kmbeyond <kiran.learn7@gmail.com>
2022-08-19 17:42:10 -0400

    Update commands_miscellaneous.sh

--- commands_miscellaneous.sh
+++ commands_miscellaneous.sh
@@ -1,5 +1,6 @@
 
 data_on_file=$(cat my_file.txt)
+#OR data_on_file=$(<my_file.txt)
 
 #return a random number between 10 & 99
 shuf -i 10-99 -n 1
@@ -7,7 +8,7 @@ shuf -i 10-99 -n 1
 
 
 
-#zip & unzip
+#------------zip & unzip
 zip km_zipped_file.zip file1.txt file2.txt
 zip km_zipped_file.zip source_dir/*
 zip -r km_zipped_file.zip .
@@ -17,7 +18,7 @@ unzip km_zipped_file.zip -d .
 unzip km_zipped_file.zip -d km_unzipped_dir/
 
 
-#split a string
+#--------------split a string
 test_string="abc_def_ghi-jkl_mno"
 
 #---using cut
@@ -36,4 +37,15 @@ echo $(echo "${test_string}" | awk -F'[_-]' '{print $3}')
 => ghi
 
 
+#--------------email
+vi email_template.txt
+Hello XFIRST_NAMEX,
+Account is setup.
 
+email_template=$(<email_template.txt)
+echo "${email_template//XFIRST_NAMEX/Kiran}" > email_kiran.txt
+
+mailx -s "test email" user@domain.com -r donotreply@domain.com < email_kiran.txt
+
+
+#----------------
