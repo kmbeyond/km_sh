ea752d955087b152f0746ab3762c7d1eeae1da5b
kmbeyond <kiran.learn7@gmail.com>
2022-08-19 17:17:15 -0400

    Create commands_miscellaneous.sh

---
+++ commands_miscellaneous.sh
@@ -0,0 +1,17 @@
+
+
+#return a random number between 10 & 99
+shuf -i 10-99 -n 1
+
+
+
+
+#zip & unzip
+zip km_zipped_file.zip file1.txt file2.txt
+zip km_zipped_file.zip source_dir/*
+zip -r km_zipped_file.zip .
+
+#unzip
+unzip km_zipped_file.zip -d .
+unzip km_zipped_file.zip -d km_unzipped_dir/
+
