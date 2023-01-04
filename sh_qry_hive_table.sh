de35465a5d7b33ab63e4f53126b9963ec4997160
kmbeyond <kiran.learn7@gmail.com>
2023-01-04 18:35:36 -0500

    Update sh_qry_hive_table.sh

--- sh_qry_hive_table.sh
+++ sh_qry_hive_table.sh
@@ -7,6 +7,53 @@ processDate=$(date '+%Y-%m-%d_%H_%M_%S')
 log_file=/tmp/km_log_${processDate}_$HOSTNAME.log
 path_hdfs_logs="/km/bd/"
 
+
+
+#--Query beeline result (one row) into a string
+
+sql_row=$(beeline -u "${hive_url}" --silent=true --showHeader=false --outputformat=csv \
+ -e "set mapreduce.job.queuename=${pool_name}; SELECT * FROM ${db_hive}.km_mrchnt LIMIT 1 ")
+
+echo ${sql_row}
+=> '1','ABC COMPANY, INC','123 Main St'
+
+#remove quotes using any of below:
+echo ${sql_row//\'/}
+#OR echo $sql_row | tr -d "'"
+#OR echo $(echo $sql_row | sed -r "s/'//g")
+=> 1,ABC COMPANY, INC,123 Main St
+
+sql_row2=${sql_row//\'/}
+
+#Split it to get each element
+IFS=',' read col1 col2 col3 <<<$sql_row2
+
+#OR: Split using cut
+col1=$(echo $sql_row2 | cut -f1 -d,)
+col2=$(echo $sql_row2 | cut -f2 -d,)
+
+
+#--Query to get multple rows
+sql_rows=$(beeline -u "${hive_url}" --silent=true --showHeader=false --outputformat=csv \
+ -e "set mapreduce.job.queuename=${pool_name}; select * from ${db_hive}.km_mrchnt")
+
+#split by new-line into array
+IFS=$'\n' data_array=($sql_rows)
+
+for str in "${data_array[@]}"; do
+   IFS=',' read col1 col2 col3 <<<$str;
+   echo " -> $str -> $col1 $col2 $col3"
+done
+=>
+ -> '1','ABC COMPANY, INC','123 Main St' -> '1' 'ABC COMPANY  INC','123 Main St'
+ -> '2','XYZ COMPANY INC.','456' -> '2' 'XYZ COMPANY INC.' '456'
+ -> '3','WALMART - 2345','Alpha Lane' -> '3' 'WALMART - 2345' 'Alpha Lane'
+
+
+
+
+
+#--insert into table
 beeline -u "${hive_url}""?mapreduce.job.queuename=${pool_name}" --silent \
  -e """
 INSERT OVERWRITE TABLE ${db_hive}.tbl_ext_tbl2 PARTITION (extract_date='${dt_today}')
@@ -18,22 +65,3 @@ if [ $retCode -ne 0 ]; then
     echo "****** ERROR: SQL for Peer compliance data" 2>&1 | tee -a ${log_file}
     exit 10
 fi
-
-
---Query beeline result into a string(s)
-sJobDates=$(beeline -u "${hive_url}""?mapreduce.job.queuename=${pool_name}"  --outputformat=csv2 --silent=true --showHeader=false --silent=true \
- -e "SELECT start_dt,end_dt FROM kmdb.kmtbl WHERE job_type='STLMNT_TRAN_DLY'")
-==> 2020-11-04,2020-11-04
-
-if [[ ${#sJobDates} -ne 21 ]]; then
-  echo "ERROR: Check Query that returns dates from kmdb.kmtbl: $sJobDates" 2>&1 | tee -a ${log_file}
-  exit 90
-fi
-
-#Split it to get each element
-IFS=',' read stlmt_from_dt stlmt_to_dt <<<$sJobDates
-
-#OR: Split using cut
-stlmt_from_dt=$(echo $sJobDates | cut -f1 -d,)
-stlmt_to_dt=$(echo $sJobDates | cut -f2 -d,)
-
