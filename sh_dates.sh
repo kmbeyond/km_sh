4f9e44342db5dc91042c294d96a385e128b9a62e
kmbeyond <kiran.learn7@gmail.com>
2023-11-02 15:53:26 -0400

    date formatting

--- sh_dates.sh
+++ sh_dates.sh
@@ -41,3 +41,38 @@ if [ $(date '+%m') == "$month_on_file" ]; then
  echo "same Month"
 fi
 
+
+
+
+
+------------------------- date formatting ----------------------------------------
+--- 20221102 --> 2022-11-02
+my_date_str="20221102"
+
+date -d $my_date_str +"%Y-%m-%d"
+
+echo ${my_date_str:0:4}-${my_date_str:4:2}-${my_date_str:6:2}
+
+
+
+--- 2022-11-02 --> 20221102
+my_date_str2="2022-11-02"
+
+date -d $my_date_str2 +"%Y%m%d"
+
+IFS=- read y m d <<< $my_date_str2
+echo "$y$m$d"
+
+IFS=- read -ra date_array <<< $my_date_str2
+echo ${date_array[0]}${date_array[1]}${date_array[2]}
+
+printf '%.4d%.2d%.2d\n' $y $m $d
+
+echo $my_date_str2 | awk -F'-' '{print $1$2$3}'
+
+y=$(echo $my_date_str2 | cut -d "-" -f1)
+m=$(echo $my_date_str2 | cut -d "-" -f2)
+d=$(echo $my_date_str2 | cut -d "-" -f1)
+echo $y$m$d
+
+
